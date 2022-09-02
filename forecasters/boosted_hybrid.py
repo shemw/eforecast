3b70c99c7d426f5dccf6266eec27b3b008a2607d
Shem Weekes <shem.weekes@upside.energy>
2022-09-02 15:57:35 +0100

    chore: refactor

--- forecasters/boosted_hybrid.py
+++ forecasters/boosted_hybrid.py
@@ -7,10 +7,8 @@ class BoostedHybrid:
     def __init__(self, model_1, model_2):
         self.model_1 = model_1
         self.model_2 = model_2
-        self.y_columns = None  # store col names from fit method
 
     # Define fit method for boosted hybrid
-
     def fit(self, X_1, X_2, y):
         self.model_1.fit(X_1, y)
         y_fit = pd.DataFrame(self.model_1.predict(X_1),
@@ -19,13 +17,9 @@ class BoostedHybrid:
 
         y_resid = y.y - y_fit.y_fit
 
-        # y_resid = y_resid.stack().squeeze()  # wide to long
-
         # fit model_2 on residuals
         self.model_2.fit(X_2, y_resid)
 
-        # save col names for predict method
-        #self.y_columns = y.columns
 
     def predict(self, X_1, X_2):
         # Define predict method for boosted hybrid
@@ -38,10 +32,8 @@ class BoostedHybrid:
                                         index=X_1.index,
                                         columns=["y_pred_residuals"])
 
-        # y_pred = y_pred.stack().squeeze()  # wide to long
-
         # add model_2 predictions
-        # TODOD sort out this indexing in a better way
+        # TODO sort out this indexing in a better way
         y_pred_boosted = (y_pred.y_pred + y_pred_residuals.y_pred_residuals).rename("y_pred_boosted")
 
         y_predictions = pd.concat([y_pred, y_pred_boosted], axis=1, ignore_index=False)
